407a56b1d532bec6f4ffde71f3e1aa9e14dc2de1
sierraskyewallace <sskwallace@gmail.com>
2021-10-17 11:12:26 -0700

    removed devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   root 'moods#index'
 
-  devise_for :users
+
 
   namespace :api do
     namespace :v1 do
+      resources :users
       resources :moods
       resources :meditation_logs
       resources :affirmations
