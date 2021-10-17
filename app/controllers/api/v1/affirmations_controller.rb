0ea529535a818070d0ed736e1105722032d6e4d1
sierraskyewallace <sskwallace@gmail.com>
2021-10-17 10:38:52 -0700

    Added new and create to affirmations

--- app/controllers/api/v1/affirmations_controller.rb
+++ app/controllers/api/v1/affirmations_controller.rb
@@ -3,6 +3,25 @@ class Api::V1::AffirmationsController < ApplicationController
 
     def index
         affirmations = Affirmation.all
-        render json: @affirmations
+        render json: AffirmationSerializer.new(affirmations)
+    end
+
+    def new 
+        affirmation = Affirmation.new
+    end
+
+    def create
+        affirmation = Affirmation.create(affirmation_params)
+        if affirmation.save!
+            render json: AffirmationSerializer.new(affirmation)
+        else
+            render json: {errors: affirmation.errors.full_messages}
+        end
+    end
+
+    private
+
+    def affirmation_params
+        params.require(:affirmation).permit(:name)
     end
 end
